b66ab22206afd38325bc7fc27bdc2c1a4c4b7457
Roger Jungemann <roger@thefifthcircuit.com>
2019-02-01 09:09:30 -0800

    Fix a failing test

--- src/app/ResolveRoute.test.js
+++ src/app/ResolveRoute.test.js
@@ -24,7 +24,7 @@ describe('routeRegex', () => {
 
 describe('resolveRoute', () => {
     const test_cases = [
-        ['/', { page: 'PostsIndex', params: ['trending'] }],
+        ['/', { page: 'PostsIndex' }],
         ['/about.html', { page: 'About' }],
         ['/faq.html', { page: 'Faq' }],
         ['/login.html', { page: 'Login' }],
