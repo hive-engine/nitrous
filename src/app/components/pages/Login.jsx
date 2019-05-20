103c632593e2a4ac2afb32370b14d4888ba0608f
roadscape <roadscape@users.noreply.github.com>
2019-05-20 14:07:05 -0500

    redirect to wallet when logging in from login.html, fix #24

--- src/app/components/pages/Login.jsx
+++ src/app/components/pages/Login.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
 import LoginForm from 'app/components/modules/LoginForm';
 import tt from 'counterpart';
 
 class Login extends React.Component {
+    componentWillMount() {
+        const { username, loggedIn } = this.props;
+        console.log('component will mount..');
+        if (loggedIn) {
+            if (process.env.BROWSER) {
+                browserHistory.replace(`/@${username}/transfers`);
+            }
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        console.log('component did update..');
+        const { username, loggedIn } = this.props;
+        if (!prevProps.loggedIn && loggedIn) {
+            if (process.env.BROWSER) {
+                browserHistory.replace(`/@${username}/transfers`);
+            }
+        }
+    }
+
     render() {
         if (!process.env.BROWSER) {
             // don't render this page on the server
@@ -24,5 +46,12 @@ class Login extends React.Component {
 
 module.exports = {
     path: 'login.html',
-    component: Login,
+    component: connect((state, ownProps) => {
+        const username = state.user.getIn(['current', 'username']);
+        const loggedIn = !!username;
+        return {
+            username,
+            loggedIn,
+        };
+    })(Login),
 };
