aff7077443b8dcf2918535b4d04126b29c60aa7e
Evan Chou <chou86.e@gmail.com>
2019-06-12 22:53:20 +0000

    lassecash configuration

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -1,24 +1,33 @@
 import { List } from 'immutable';
 
 // sometimes it's impossible to use html tags to style coin name, hence usage of _UPPERCASE modifier
-export const APP_NAME = 'WeedCash';
+export const APP_NAME = 'LasseCash';
 // sometimes APP_NAME is written in non-latin characters, but they are needed for technical purposes
 // ie. "Голос" > "Golos"
-export const APP_NAME_LATIN = 'WeedCash';
-export const APP_NAME_UPPERCASE = 'WEEDCASH';
-export const APP_ICON = 'weedcash';
+export const APP_NAME_LATIN = 'LasseCash';
+export const APP_NAME_UPPERCASE = 'LASSECASH';
+export const APP_ICON = 'lassecash';
 // FIXME figure out best way to do this on both client and server from env
 // vars. client should read $STM_Config, server should read config package.
-export const APP_URL = 'https://www.weedcash.network';
-export const APP_DOMAIN = 'www.weedcash.network';
-export const SCOT_TAG = 'weedcash';
-export const TAG_LIST = List(['weedcash', 'weed', 'cannabis', 'psychedelic']);
-export const LIQUID_TOKEN = 'Weed';
+export const APP_URL = 'https://www.lassecash.com';
+export const APP_DOMAIN = 'www.lassecash.com';
+export const SCOT_TAG = 'lassecash';
+export const TAG_LIST = List([
+    'lassecash',
+    'anarchocapitalism',
+    'cryptocurrencies',
+    'flatearth',
+    'personalpost',
+    'design',
+    'pictures',
+    'life',
+]);
+export const LIQUID_TOKEN = 'Lassecash';
 // sometimes it's impossible to use html tags to style coin name, hence usage of _UPPERCASE modifier
-export const LIQUID_TOKEN_UPPERCASE = 'WEED';
+export const LIQUID_TOKEN_UPPERCASE = 'LASSECASH';
 export const SCOT_DENOM = 100000000;
 export const VOTE_WEIGHT_DROPDOWN_THRESHOLD = 1;
-export const VESTING_TOKEN = 'WEED POWER';
+export const VESTING_TOKEN = 'LASSE POWER';
 export const INTERLEAVE_PROMOTED = true;
 export const PROMOTED_POST_ACCOUNT = 'null';
 
@@ -51,8 +60,8 @@ export const SHARE_IMAGE =
 export const TWITTER_SHARE_IMAGE =
     'https://' + APP_DOMAIN + '/images/steemit-twshare.png';
 export const SITE_DESCRIPTION =
-    'Weedcash is a social media platform where everyone gets paid for ' +
-    'creating and curating content. It leverages a robust digital points system, called WEED, that ' +
+    'LasseCash is a social media platform where everyone gets paid for ' +
+    'creating and curating content. It leverages a robust digital points system, called LASSECASH, that ' +
     'supports real value for digital rewards through market price discovery and liquidity';
 
 // various
