67d88cf5aef5843ba6f59e630d2488f11bc8a6cd
Segye Park <iworldpark@gmail.com>
2019-06-23 03:43:01 +0900

    Move fontawesome kit position as recommended

--- src/server/server-html.jsx
+++ src/server/server-html.jsx
@@ -195,6 +195,7 @@ export default function ServerHTML({
                         src={`https://load.fomo.com/ads/load.js?id=${fomoId}`}
                     />
                 ) : null}
+                <script src="https://kit.fontawesome.com/c2c7de72e5.js" />
                 <title>{page_title}</title>
             </head>
             <body>
@@ -207,7 +208,6 @@ export default function ServerHTML({
                 {assets.script.map((href, idx) => (
                     <script key={idx} src={href} />
                 ))}
-                <script src="https://kit.fontawesome.com/c2c7de72e5.js" />
             </body>
         </html>
     );
