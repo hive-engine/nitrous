c7211d096076096657d94f355972fb4cb4f92cf1
thebluewin <52869419+thebluewin@users.noreply.github.com>
2019-07-14 16:35:07 +0200

    Update SidebarLinks.jsx

--- src/app/components/elements/SidebarLinks.jsx
+++ src/app/components/elements/SidebarLinks.jsx
@@ -27,6 +27,15 @@ const SidebarLinks = ({ username }) => (
                         {tt('g.my_wallet')}
                     </a>
                 </li>
+                
+                <li className="c-sidebar__list-item">
+                    <a
+                        className="c-sidebar__link"
+                        href="https://tube.steemace.io"
+                    >
+                        AceTube
+                    </a>
+                </li>
             </ul>
         </div>
     </div>
