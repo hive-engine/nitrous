7b7ecd194a631fd76e44a69ca0268451eeb84b05
Evan Chou <chou86.e@gmail.com>
2019-07-17 02:30:27 +0000

    fix tests

--- src/app/redux/MarketReducer.test.js
+++ src/app/redux/MarketReducer.test.js
@@ -14,8 +14,8 @@ const someOpenOrders = [
         orderid: 1512431634,
         for_sale: 3000,
         sell_price: {
-            base: '3.000 STEEM',
-            quote: '1.998 SBD',
+            base: '3.000 TESTS',
+            quote: '1.998 TBD',
         },
         real_price: '0',
         rewarded: false,
@@ -28,8 +28,8 @@ const someOpenOrders = [
         orderid: 1512431689,
         for_sale: 7000,
         sell_price: {
-            base: '7.000 STEEM',
-            quote: '4.578 SBD',
+            base: '7.000 TESTS',
+            quote: '4.578 TBD',
         },
         real_price: '0',
         rewarded: false,
@@ -42,8 +42,8 @@ const someOpenOrders = [
         orderid: 1512431835,
         for_sale: 3000,
         sell_price: {
-            base: '3.000 STEEM',
-            quote: '1.953 SBD',
+            base: '3.000 TESTS',
+            quote: '1.953 TBD',
         },
         real_price: '0',
         rewarded: false,
@@ -56,8 +56,8 @@ const someOpenOrders = [
         orderid: 1512492109,
         for_sale: 507,
         sell_price: {
-            base: '0.507 SBD',
-            quote: '1.000 STEEM',
+            base: '0.507 TBD',
+            quote: '1.000 TESTS',
         },
         real_price: '0',
         rewarded: false,
@@ -70,8 +70,8 @@ const someOpenOrders = [
         orderid: 1512492144,
         for_sale: 507,
         sell_price: {
-            base: '0.507 SBD',
-            quote: '1.000 STEEM',
+            base: '0.507 TBD',
+            quote: '1.000 TESTS',
         },
         real_price: '0',
         rewarded: false,
@@ -106,10 +106,10 @@ describe('market reducer', () => {
         const orders = withOrders.get('open_orders');
 
         expect(orders[0].price).toBe(0.666);
-        expect(orders[0].sbd).toBe('1.998 SBD');
+        expect(orders[0].sbd).toBe('1.998 TBD');
         expect(orders[1].type).toBe('ask');
         expect(orders[2].price).toBe(0.651);
-        expect(orders[2].sbd).toBe('1.953 SBD');
+        expect(orders[2].sbd).toBe('1.953 TBD');
         expect(orders[3].type).toBe('bid');
         expect(orders[3].type).toBe('bid');
     });
