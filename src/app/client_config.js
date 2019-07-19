86d1bcf33b34ea0d8df9235d8cd269f1f242fb52
Evan Chou <chou86.e@gmail.com>
2019-07-19 23:06:43 +0000

    int config and files

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -1,25 +1,25 @@
 import { List } from 'immutable';
 
 // sometimes it's impossible to use html tags to style coin name, hence usage of _UPPERCASE modifier
-export const APP_NAME = 'WeedCash';
+export const APP_NAME = 'Intrepreneur';
 // sometimes APP_NAME is written in non-latin characters, but they are needed for technical purposes
 // ie. "Голос" > "Golos"
-export const APP_NAME_LATIN = 'WeedCash';
-export const APP_NAME_UPPERCASE = 'WEEDCASH';
-export const APP_ICON = 'weedcash';
+export const APP_NAME_LATIN = 'Intrepreneur';
+export const APP_NAME_UPPERCASE = 'INTREPRENEUR';
+export const APP_ICON = 'intrepreneur';
 // FIXME figure out best way to do this on both client and server from env
 // vars. client should read $STM_Config, server should read config package.
-export const APP_URL = 'https://www.weedcash.network';
-export const APP_DOMAIN = 'www.weedcash.network';
-export const SCOT_TAG = 'weedcash';
-export const TAG_LIST = List(['weedcash', 'weed', 'cannabis', 'psychedelic']);
-export const LIQUID_TOKEN = 'Weed';
+export const APP_URL = 'https://www.intrepreneur.life';
+export const APP_DOMAIN = 'www.intrepreneur.life';
+export const SCOT_TAG = 'int';
+export const TAG_LIST = List(['int']);
+export const LIQUID_TOKEN = 'Int';
 // sometimes it's impossible to use html tags to style coin name, hence usage of _UPPERCASE modifier
-export const LIQUID_TOKEN_UPPERCASE = 'WEED';
+export const LIQUID_TOKEN_UPPERCASE = 'INT';
 // used as backup
-export const SCOT_DENOM = 100000000;
+export const SCOT_DENOM = 10000;
 export const VOTE_WEIGHT_DROPDOWN_THRESHOLD = 1;
-export const VESTING_TOKEN = 'WEED POWER';
+export const VESTING_TOKEN = 'INT POWER';
 export const INTERLEAVE_PROMOTED = true;
 export const PROMOTED_POST_ACCOUNT = 'null';
 
@@ -52,8 +52,8 @@ export const SHARE_IMAGE =
 export const TWITTER_SHARE_IMAGE =
     'https://' + APP_DOMAIN + '/images/steemit-twshare.png';
 export const SITE_DESCRIPTION =
-    'Weedcash is a social media platform where everyone gets paid for ' +
-    'creating and curating content. It leverages a robust digital points system, called WEED, that ' +
+    'Intrepreneur is a social media platform where everyone gets paid for ' +
+    'creating and curating content. It leverages a robust digital points system, called INT, that ' +
     'supports real value for digital rewards through market price discovery and liquidity';
 
 // various
