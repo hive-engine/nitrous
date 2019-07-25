207893617cfd1fa71fdf4613555a54f75dceeb24
thebluewin <52869419+thebluewin@users.noreply.github.com>
2019-07-25 18:22:31 +0200

    Update client_config.js

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -12,14 +12,12 @@ export const APP_ICON = 'steemace';
 export const APP_URL = 'https://www.steemace.io/';
 export const APP_DOMAIN = 'www.steemace.io';
 export const SCOT_TAG = 'steemace';
-export const TAG_LIST = fromJSOrdered({
-    gaming: ['gamingreview', 'letsplay', 'esports', 'news', 'vr', 'mobilegame'],
-    cryptogames: ['splinterlands', 'nextcolony', 'radaquest', 'roadofrich'],
-    boardgames: ['cardgames', 'rpg', 'penandpaper' 'poker'],
-    giveaway: ['contest'],
-    platforms: ['dtube', 'vimmtv', '3speak'],
-    language: ['spanish', 'deutsch', 'kr', 'cn', 'polish', 'japanese'],
-});
+export const TAG_LIST = fromJSOrdered([
+    'gaming',
+    'boardgames',
+    'splinterlands',
+    'nextcolony',
+]);
 export const LIQUID_TOKEN = 'GG';
 // sometimes it's impossible to use html tags to style coin name, hence usage of _UPPERCASE modifier
 export const LIQUID_TOKEN_UPPERCASE = 'GG';
