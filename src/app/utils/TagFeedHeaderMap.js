c7c26f5731e0e3fd31f2f427f19db1ecff165f82
surpassinggoogle <comsilbronze4@gmail.com>
2019-07-26 20:37:26 +0800

    Update TagFeedHeaderMap.js

--- src/app/utils/TagFeedHeaderMap.js
+++ src/app/utils/TagFeedHeaderMap.js
@@ -1,7 +1,7 @@
 const map = {
     // default
     '':
-        "Our communities are overseen by *'certified uloggers'* only. Indicate your interest in overseeing this community **[here](https://www.marlians.com/marlians/@surpassinggoogle/simplified-process-for-becoming-a-certified-ulogger-now-4-very-simple-steps-only-chinese-and-korean-translation-available)**`",
+        "Our communities are overseen by *'certified uloggers'* only. Get *'certified'* **[here](https://www.marlians.com/marlians/@surpassinggoogle/simplified-process-for-becoming-a-certified-ulogger-now-4-very-simple-steps-only-chinese-and-korean-translation-available)**`; then, indicate your interest in overseeing a community by contacting '@surpassinggoogle#1660' on [discord](https://discord.gg/nHVV5e9)",
 
     ulog:
         "All forms of content, spanning over every niche allowed here, as long as all aspects of the content like text, videos, images, graphics etc was created 'fresh' and entirely by U, without resourcing from the internet. For help, contact [@snook](https://www.marlians.com/@snook), [@surpassinggoogle](https://www.marlians.com/@surpassinggoogle), [@dreemsteem](https://www.marlians.com/@dreemsteem) or [@anikys3reasure](https://www.marlians.com/@anikys3reasure) **[here!](https://discord.gg/nHVV5e9)**",
