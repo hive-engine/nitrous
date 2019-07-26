ff813c09601630524230e2c002f69a6acafd7524
Evan Chou <chou86.e@gmail.com>
2019-07-26 03:22:13 +0000

    logo size

--- src/app/components/elements/AppLogo/index.jsx
+++ src/app/components/elements/AppLogo/index.jsx
@@ -4,7 +4,7 @@ import SvgImage from 'app/components/elements/SvgImage';
 import { APP_ICON } from 'app/client_config';
 
 const AppLogo = () => {
-    return <SvgImage name={APP_ICON} width="150px" height="40px" />;
+    return <SvgImage name={APP_ICON} width="80px" height="40px" />;
 };
 
 export default AppLogo;
