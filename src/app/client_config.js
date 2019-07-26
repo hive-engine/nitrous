cbf6ec34c1b4838d503c77c532744014b42335e9
Evan Chou <chou86.e@gmail.com>
2019-07-26 03:06:25 +0000

    initial assets, settings

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -1,30 +1,25 @@
 import { fromJSOrdered } from './utils/immutable';
 
 // sometimes it's impossible to use html tags to style coin name, hence usage of _UPPERCASE modifier
-export const APP_NAME = 'WeedCash';
+export const APP_NAME = 'DPorn';
 // sometimes APP_NAME is written in non-latin characters, but they are needed for technical purposes
 // ie. "Голос" > "Golos"
-export const APP_NAME_LATIN = 'WeedCash';
-export const APP_NAME_UPPERCASE = 'WEEDCASH';
-export const APP_ICON = 'weedcash';
+export const APP_NAME_LATIN = 'DPorn';
+export const APP_NAME_UPPERCASE = 'DPORN';
+export const APP_ICON = 'dporn';
 // FIXME figure out best way to do this on both client and server from env
 // vars. client should read $STM_Config, server should read config package.
-export const APP_URL = 'https://www.weedcash.network';
-export const APP_DOMAIN = 'www.weedcash.network';
-export const SCOT_TAG = 'weedcash';
-export const TAG_LIST = fromJSOrdered([
-    'weedcash',
-    'weed',
-    'cannabis',
-    'psychedelic',
-]);
-export const LIQUID_TOKEN = 'Weed';
+export const APP_URL = 'https://www.dporn.co';
+export const APP_DOMAIN = 'www.dporn.co';
+export const SCOT_TAG = 'porn';
+export const TAG_LIST = fromJSOrdered(['porn']);
+export const LIQUID_TOKEN = 'Porn';
 // sometimes it's impossible to use html tags to style coin name, hence usage of _UPPERCASE modifier
-export const LIQUID_TOKEN_UPPERCASE = 'WEED';
+export const LIQUID_TOKEN_UPPERCASE = 'PORN';
 // used as backup
 export const SCOT_DENOM = 100000000;
 export const VOTE_WEIGHT_DROPDOWN_THRESHOLD = 1;
-export const VESTING_TOKEN = 'WEED POWER';
+export const VESTING_TOKEN = 'PORN POWER';
 export const INTERLEAVE_PROMOTED = true;
 export const PROMOTED_POST_ACCOUNT = 'null';
 
@@ -57,8 +52,8 @@ export const SHARE_IMAGE =
 export const TWITTER_SHARE_IMAGE =
     'https://' + APP_DOMAIN + '/images/steemit-twshare.png';
 export const SITE_DESCRIPTION =
-    'Weedcash is a social media platform where everyone gets paid for ' +
-    'creating and curating content. It leverages a robust digital points system, called WEED, that ' +
+    'DPorn is a social media platform where everyone gets paid for ' +
+    'creating and curating content. It leverages a robust digital points system, called PORN, that ' +
     'supports real value for digital rewards through market price discovery and liquidity';
 
 // various
