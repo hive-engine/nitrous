df560616e8e7d77131f14b45f1abf7b0f6c7b16f
Neo-xian <mbowen@mykolab.com>
2019-07-25 19:55:10 -0500

    Update AffiliationMap.js

--- src/app/utils/AffiliationMap.js
+++ src/app/utils/AffiliationMap.js
@@ -13,6 +13,15 @@ const map = {
 
     //neoxag
     neoxian: 'founder',
+    zaku: 'tribe_team',
+    news-today: 'tribe_team',
+    reazuliqbal: 'tribe_team',
+    bala41288: 'tribe_team',
+    thereikiforest: 'pope',
+    r2cornell: 'dragon',
+    scipio: 'dragon',
+    afolwalex: 'dragon',
+    bullionstackers: 'dragon',
 };
 
 export default map;
