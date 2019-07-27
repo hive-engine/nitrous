016903607d3262a31f8b1d64276a2a550b98e312
thebluewin <52869419+thebluewin@users.noreply.github.com>
2019-07-27 12:55:43 +0200

    Update client_config.js

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -15,7 +15,7 @@ export const SCOT_TAG = 'steemace';
 export const TAG_LIST = fromJSOrdered({
     gaming: ['gamingreview', 'letsplay', 'esports', 'news', 'vr', 'mobilegame'],
     cryptogames: ['splinterlands', 'nextcolony', 'radaquest', 'roadofrich'],
-    tabletop: ['rpg', 'boardgame', 'cardgame', 'poker'],
+    tabletop: ['tabletop-rpg', 'boardgame', 'cardgame', 'poker'],
     giveaway: ['contest'],
     platforms: ['dtube', 'vimmtv', 'threespeak'],
     language: ['spanish', 'deutsch', 'kr', 'cn', 'polish', 'japanese'],
