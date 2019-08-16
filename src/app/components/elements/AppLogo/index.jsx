87b34a899caadd08ef43c717294300adaf487976
eonwarped <34953718+eonwarped@users.noreply.github.com>
2019-08-16 14:14:39 -0400

    Update index.jsx

--- src/app/components/elements/AppLogo/index.jsx
+++ src/app/components/elements/AppLogo/index.jsx
@@ -4,7 +4,7 @@ import SvgImage from 'app/components/elements/SvgImage';
 import { APP_ICON } from 'app/client_config';
 
 const AppLogo = () => {
-    return <SvgImage name={APP_ICON} width="150px" height="40px" />;
+    return <SvgImage name={APP_ICON} width="55px" height="45px" />;
 };
 
 export default AppLogo;
