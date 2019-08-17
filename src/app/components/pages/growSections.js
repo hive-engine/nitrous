ae228b2a9363f2475384abfbb824536bc868ded8
surpassinggoogle <comsilbronze4@gmail.com>
2019-08-17 20:38:24 +0800

    Update growSections.js

--- src/app/components/pages/growSections.js
+++ src/app/components/pages/growSections.js
@@ -1,10 +1,10 @@
 export const aboutGrow = `
-On Ulogs.org, it is **"true celebrity-hood for everyone, once and for all"**. We measure our success in terms of **'real human growth'**. We want to create **more value for 'humans'** in a world were cryptokitties, superman, millionaire-cats, human-clones etc is beginning to garner more value than 'humans'.
+On Marlians.com, it is **"true celebrity-hood for everyone, once and for all"**. We measure our success in terms of **'real human growth'**. We want to create **more value for 'humans'** in a world were cryptokitties, superman, millionaire-cats, human-clones etc is beginning to garner more value than 'humans'.
 
 &nbsp;
 &nbsp;
 
-Thus, we will incessantly seek innovative ways of inciting each ulogger to **'mine the human' into its awesomest version**. In the future, we will seek to celebrate a world filled with **'true celebrities'**, using the [TearDrops SMT](https://ulogs.org/teardrops/@surpassinggoogle/the-teardrops-smt-is-still-in-its-imaginary-state-but-it-is-time-to-start-gathering-read-this-and-consider-joining-our-telegram), 'a breakthrough token & emblem of human' that will reward 'proof of tears', for en-route attaining the awesomest version of human, there will be many tears, happy, sad or un-fell.
+Thus, we will incessantly seek innovative ways of inciting each user to **'mine the human' into its awesomest version**. In the future, we will seek to celebrate a world filled with **'true celebrities'**, using the [MARLIANS](https://steem-engine.com/?p=market&t=MARLIANS) tokens too, rewarding the resulting 'breakthrough in human'.
 
 &nbsp;
 &nbsp;
@@ -13,12 +13,12 @@ We seek to remove 'all barriers to entry' from 'everything good', so that every(
 `;
 
 export const submitAVideo = `
-Across the ulogs.org ecosystem, we have 3 TV(s) i.e [UloggersTV](https://www.youtube.com/channel/UCzI3Rjamg7zSe_o0BwSeIQQ), [UntalentedTV](https://www.youtube.com/channel/UCq6ylwOaSG9VDYwZkMr3jMw) and [TeardropsTV](https://www.youtube.com/channel/UCnIJlzMZzfrcE0bXabLpXKw). We seek to use the media to impact the world and you can help matters. **We welcome your contributions**. We will feature your contribution on a corresponding TV. **In the future, your contributions may feature in our Uloggers Game App**.  
+Across our ecosystem, we have 4 TV(s) i.e [Marlians](https://www.youtube.com/channel/UCH4tvTSFkjn3KqNQfJVWITQ), [UloggersTV](https://www.youtube.com/channel/UCzI3Rjamg7zSe_o0BwSeIQQ), [UntalentedTV](https://www.youtube.com/channel/UCq6ylwOaSG9VDYwZkMr3jMw) and [TeardropsTV](https://www.youtube.com/channel/UCnIJlzMZzfrcE0bXabLpXKw). We seek to use the media from each TV to impact the world and you can help matters. **We welcome your contributions**. We will feature your contribution on a corresponding TV. **In the future, your contributions may feature in our Uloggers Game App**.  
 
 &nbsp;
 &nbsp;
 
-While we only feature *'certified uloggers'* on our TV(s), if you are not yet a [*'certified ulogger'*](https://ulogs.org/discover) and you submit a video, **we will also look to feature your video** and once we do so, **you will become a 'certified ulogger'**.
+While we only feature *'certified uloggers'* on our TV(s), if you are not yet a [*'certified ulogger'*](https://ulogs.org/discover) and you submit a video, **we will also look to feature your video** and once we do so, **we will consider giving you a 'certified' status**.
 `;
 
 export const writeToUlogs = `
@@ -40,7 +40,7 @@ Many times, we **rigamarole for years** seeking that *'ever-missing piece of the
 `;
 
 export const thirtyMinuteSymposium = `
-**The ulogs.org ecosystem seeks to unite every steem community**, creating balance in the steem ecosystem. We are doing this using [ulog-subtags](https://ulogs.org/utopian-io/@surpassinggoogle/task-request-add-core-features-and-communities-to-ulogs-org-880-steem-bounty-and-more), which in turn grow into [ulog-communities](https://ulogs.org/utopian-io/@surpassinggoogle/task-request-add-core-features-and-communities-to-ulogs-org-880-steem-bounty-and-more) e.g [#ulog-helpie](https://ulogs.org/ulog-helpie), [#ulog-steemmonsters](https://ulogs.org/ulog-steemmonsters), [#ulog-actifit](https://ulogs.org/ulog-actifit) etc 
+**Our ecosystem seeks to unite every steem community**, creating balance in the steem ecosystem. We are doing this by creating 'mini-communities' for every steem-community right on 'marlians.com' e.g [#creativecoin](https://www.marlians.com/created/creativecoin), [#utopian-io](https://www.marlians.com/created/utopian-io), [#actifit](https://www.marlians.com/created/actifit) etc and designating some stake to empower and grow these communities.
 
 &nbsp;
 &nbsp;
@@ -49,26 +49,26 @@ We will try to help you get a **time allotment** on any of the popular steem com
 `;
 
 export const extraClout = `
-*You are a celebrity and we are your fan.* As we continue to **grow our network of 'certified uloggers'**, we may find some who have social influence whether on steem or outside steem. **Extra clout** goes a long way in creating awareness for your projects, dreams or you. **Clout** can take different forms too e.g a certified ulogger with some 1.7 million YouTube subscribers may be willing to give your product a shoutout on one of his viral videos etc. 
+*You are a celebrity and we are your fan.* As we continue to **grow our network of 'certified' users**, we may find some who have social influence whether on steem or outside steem. **Extra clout** goes a long way in creating awareness for your projects, dreams or you. **Clout** can take different forms too e.g a certified user with some 1.7 million YouTube subscribers may be willing to give your product a shoutout on one of his viral videos etc. 
 
 &nbsp;
 &nbsp;
 
-Check out our list to **find out what our 'certified uloggers' have in stock for you**.
+Check out our list to **find out what our 'certified' users have in stock for you**.
 `;
 
 export const extraConfidence = `
-Relegate reservations and reach out to us. We will seek ways to boost your confidence. [#ulog](https://ulogs.org/) represents a daily endeavor. On ulogs.org, each day carries its own freshness. **Thus, we are dynamically looking for ways to be your 'true fan'.**
+Relegate reservations and reach out to us. We will seek ways to boost your confidence. Marlians.com represents 'a home' and it is yours. **Thus, we are dynamically looking for ways to be your 'true fan'.**
 `;
 
 export const hardToExplainAilment = `
-It is worse kind of ailment, when it is a hard-to-explain ailment. **We understand.** We are growing a network of 'certified uloggers' who may understand. Reach out to us freely. **Please do!**
+It is worse kind of ailment, when it is 'a hard-to-explain ailment'. **We understand.** We are growing a network of 'certified' uloggers who may understand. Reach out to us freely. **Please do!**
 `;
 
 export const sendUsSomething = `
-**You may want to send us something.** *We will give your our mailing address*. **We want to receive, for 'receiving is an art'.**
+**You may want to send us something.** *We will give you our mailing address*. **We want to receive too; for 'receiving is an art'.**
 `;
 
 export const inspireUs = `
-[#ulog](https://ulogs.org/) is a conscious effort to 'mine the human' into its awesomest version. This has to be a daily endeavor. You can inspire us. We need it too. Send us [ulogifs](https://ulogs.org/created/ulogifs), [ulog-memes](https://ulogs.org/created/ulog-memes), [ulog-quotes](https://ulogs.org/created/ulog-quotes), [ulography](https://ulogs.org/created/ulography) etc for the sole intention of inspiring us. **Courtesy of your own shine, we can find some shine too!**
+Marlians.com is modeled to each incite users to 'mine their human'. We seek to return 'value' to each human. You can inspire us! We need it. Send us [ulogifs](https://ulogs.org/created/ulogifs), [ulog-memes](https://ulogs.org/created/ulog-memes), [ulog-quotes](https://ulogs.org/created/ulog-quotes), [ulography](https://ulogs.org/created/ulography) etc for the sole intention of inspiring us. **Courtesy of your own shine, we can find some shine too!**
 `;
