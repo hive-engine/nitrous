87f399582a0298f1b435b9ff7a9b0d9f64fa19b3
aggroed <aggroed.lighthacker@gmail.com>
2019-08-21 19:54:53 -0400

    Update AffiliationMap.js

--- src/app/utils/AffiliationMap.js
+++ src/app/utils/AffiliationMap.js
@@ -15,6 +15,7 @@ const map = {
     // 'robot.pay' : 'Robot',
     aggroed: 'PALnet Founder, Witness, MSP Waves, Keychain, Splinterlands, Steem Engine, Steem Foundation',
     ausbitbank: 'Witness, PALnet Founder, Mad Cunt',
+    r0nd0n: 'an exceptionally talented clown who has facilitated the development of the MSP Waves interactive broadcast station to the international powerhouse it has become.  We love him and his clown antics.  He is a beautiful man worthy of love, praise, and affection.    Many blessings of peace, abundance, liberty, and recreational nukes to you sir.',
 };
 
 export default map;
