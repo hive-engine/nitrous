a4b7aa4959a6a771d3967509b9672f4612df1471
eonwarped <34953718+eonwarped@users.noreply.github.com>
2019-08-28 17:40:11 -0400

    Update index.jsx

--- src/app/components/modules/SidePanel/index.jsx
+++ src/app/components/modules/SidePanel/index.jsx
@@ -113,25 +113,9 @@ const SidePanel = ({
         ],
         organizational: [
             {
-                value: 'bluepaper',
-                label: tt('navigation.bluepaper'),
-                link: 'https://steem.io/steem-bluepaper.pdf',
-            },
-            {
-                value: 'smt_whitepaper',
-                label: tt('navigation.smt_whitepaper'),
-                link: 'https://smt.steem.io/',
-            },
-            {
-                value: 'whitepaper',
-                label: tt('navigation.whitepaper'),
-                link: 'https://steem.io/SteemWhitePaper.pdf',
-            },
-            {
-                value: 'about',
-                label: tt('navigation.about'),
-                link: '/about.html',
-                internal: true,
+                value: 'lagotube',
+                label: 'LagoTube',
+                link: 'https://tube.lago.com.gt',
             },
         ],
         legal: [
@@ -171,6 +155,14 @@ const SidePanel = ({
                 <CloseButton onClick={hideSidePanel} />
                 <ul className={`vertical menu ${loggedIn}`}>
                     {sidePanelLinks['extras'].map(makeLink)}
+                </ul>
+                <ul className="vertical menu">
+                    <li>
+                        <a className="menu-section">
+                            Community
+                        </a>
+                    </li>
+                    {sidePanelLinks['organizational'].map(makeLink)}
                 </ul>
                 <ul className="vertical menu">
                     <li>
