e1c75aa819dfa763e42640df50f8be3d88f80ca7
steemleo <52357015+steemleo@users.noreply.github.com>
2019-09-15 17:45:48 -0600

    Update TagFeedHeaderMap.js

--- src/app/utils/TagFeedHeaderMap.js
+++ src/app/utils/TagFeedHeaderMap.js
@@ -1,21 +1,47 @@
 const map = {
     // default
     '':
-        "<!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-  <div class="tradingview-widget-container__widget"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/FX-EURUSD/" rel="noopener" target="_blank"><span class="blue-text">EURUSD Rates</span></a> by TradingView</div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-single-quote.js" async>
-  {
-  "symbol": "FX:EURUSD",
-  "width": 350,
-  "colorTheme": "light",
-  "isTransparent": false,
-  "locale": "en"
-}
-  </script>
-</div>
-<!-- TradingView Widget END -->"
+        "<!DOCTYPE html>
+        <html>
+        <body>
+
+        <div class="tradingview-widget-container">
+            <div class="tradingview-widget-container__widget"></div>
+            <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com" rel="noopener" target="_blank">           <span class="blue-text">Ticker Tape</span></a> by TradingView</div>
+            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+            {
+            "symbols": [
+             {
+                "title": "S&P 500",
+                "proName": "OANDA:SPX500USD"
+             },
+             {
+                "title": "Nasdaq 100",
+                "proName": "OANDA:NAS100USD"
+             },
+             {
+                "title": "EUR/USD",
+                "proName": "FX_IDC:EURUSD"
+             },
+             {
+                "title": "BTC/USD",
+                "proName": "BITSTAMP:BTCUSD"
+             },
+             {
+                "title": "ETH/USD",
+                "proName": "BITSTAMP:ETHUSD"
+             }
+           ],
+           "colorTheme": "light",
+           "isTransparent": false,
+           "displayMode": "adaptive",
+           "locale": "en"
+        }
+           </script>
+        </div>",
+
+</body>
+</html>
 
     trending:
         "trending test",
