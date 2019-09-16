fd2f7ad54a3ca950d3dabb19e4064c7e2b6f96bd
steemleo <52357015+steemleo@users.noreply.github.com>
2019-09-15 18:14:55 -0600

    Update TagFeedHeaderMap.js

--- src/app/utils/TagFeedHeaderMap.js
+++ src/app/utils/TagFeedHeaderMap.js
@@ -2,51 +2,22 @@ const map = {
     // default
     '':
         "<!DOCTYPE html>
-        <html>
-        <body>
+<html>
+<body>
 
-        <div class="tradingview-widget-container">
-            <div class="tradingview-widget-container__widget"></div>
-            <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com" rel="noopener" target="_blank">           <span class="blue-text">Ticker Tape</span></a> by TradingView</div>
-            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-            {
-            "symbols": [
-             {
-                "title": "S&P 500",
-                "proName": "OANDA:SPX500USD"
-             },
-             {
-                "title": "Nasdaq 100",
-                "proName": "OANDA:NAS100USD"
-             },
-             {
-                "title": "EUR/USD",
-                "proName": "FX_IDC:EURUSD"
-             },
-             {
-                "title": "BTC/USD",
-                "proName": "BITSTAMP:BTCUSD"
-             },
-             {
-                "title": "ETH/USD",
-                "proName": "BITSTAMP:ETHUSD"
-             }
-           ],
-           "colorTheme": "light",
-           "isTransparent": false,
-           "displayMode": "adaptive",
-           "locale": "en"
-        }
-           </script>
-        </div>
+<div style="background-color:lightblue">
+  <h3>test</h3>
+  <p>ptest</p>
+</div>
 
 </body>
-</html>",
+</html>
+",
 
-    trending:
-        "trending test 2",
-    hot:
-        "hot test",
+    dailyleo:
+        "TESTthedailyleo1",
+    learnleo:
+        "TESTthedailyleo1",
 };
 
 export default map;
