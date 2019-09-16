8bc860527422a6b744d586455ea52d49b4ea2cd4
steemleo <52357015+steemleo@users.noreply.github.com>
2019-09-15 18:36:08 -0600

    Update TagFeedHeaderMap.js

--- src/app/utils/TagFeedHeaderMap.js
+++ src/app/utils/TagFeedHeaderMap.js
@@ -1,18 +1,7 @@
 const map = {
     // default
     '':
-        "<!DOCTYPE html>
-<html>
-<body>
-
-<div style="background-color:lightblue">
-  <h3>test</h3>
-  <p>ptest</p>
-</div>
-
-</body>
-</html>
-",
+        "test2",
 
     dailyleo:
         "TESTthedailyleo1",
