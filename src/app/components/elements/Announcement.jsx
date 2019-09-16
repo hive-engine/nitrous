6a9853641d29e0a66a4db39d22f2018553d0fcaa
steemleo <52357015+steemleo@users.noreply.github.com>
2019-09-15 21:56:09 -0600

    Update Announcement.jsx

--- src/app/components/elements/Announcement.jsx
+++ src/app/components/elements/Announcement.jsx
@@ -3,40 +3,18 @@ import React from 'react';
 
 export const Announcement = ({ onClose }) => (
     <div className="annoucement-banner">
-        <div class="tradingview-widget-container">
-            <div class="tradingview-widget-container__widget"></div>
-            <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com" rel="noopener" target="_blank">           <span class="blue-text">Ticker Tape</span></a> by TradingView</div>
-            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-            {
-            "symbols": [
-             {
-                "title": "S&P 500",
-                "proName": "OANDA:SPX500USD"
-             },
-             {
-                "title": "Nasdaq 100",
-                "proName": "OANDA:NAS100USD"
-             },
-             {
-                "title": "EUR/USD",
-                "proName": "FX_IDC:EURUSD"
-             },
-             {
-                "title": "BTC/USD",
-                "proName": "BITSTAMP:BTCUSD"
-             },
-             {
-                "title": "ETH/USD",
-                "proName": "BITSTAMP:ETHUSD"
-             }
-           ],
-           "colorTheme": "light",
-           "isTransparent": false,
-           "displayMode": Im"adaptive",
-           "locale": "en"
-        }
-           </script>
-        </div>
+        <p className="announcement-banner__text">
+            this is a banner {' '}
+            <a
+                className="announcement-banner__link"
+                href="https://steemleo.com/"
+            >
+                test
+            </a>
+        </p>
+        <button className="close-button" type="button" onClick={onClose}>
+            &times;
+        </button>
     </div>
 );
 
