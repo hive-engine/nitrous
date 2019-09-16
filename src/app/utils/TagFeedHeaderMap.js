a146c97beb47494234e4c907fe69394d12ba84da
steemleo <52357015+steemleo@users.noreply.github.com>
2019-09-16 00:11:00 -0600

    Update TagFeedHeaderMap.js

--- src/app/utils/TagFeedHeaderMap.js
+++ src/app/utils/TagFeedHeaderMap.js
@@ -1,12 +1,12 @@
 const map = {
     // default
     '':
-        "test6",
+        "",
 
     dailyleo:
-        "test6",
+        "",
     learnleo:
-        "test6",
+        "",
 };
 
 export default map;
