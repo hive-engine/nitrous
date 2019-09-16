d984622d397b2d2e190d36d86a4812606f1d9341
steemleo <52357015+steemleo@users.noreply.github.com>
2019-09-16 11:40:53 -0600

    Update About.jsx

--- src/app/components/pages/About.jsx
+++ src/app/components/pages/About.jsx
@@ -22,29 +22,6 @@ class About extends React.Component {
                                     with financial inclusion.
                                 </span>
                             </p>
-                            <div class="tradingview-widget-container">
-                              <div id="tradingview_6446d"></div>
-                              <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
-                              <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-                              <script type="text/javascript">
-                              new TradingView.widget(
-                              {
-                              "width": 980,
-                              "height": 610,
-                              "symbol": "NASDAQ:AAPL",
-                              "interval": "D",
-                              "timezone": "Etc/UTC",
-                              "theme": "Light",
-                              "style": "1",
-                              "locale": "en",
-                              "toolbar_bg": "#f1f3f6",
-                              "enable_publishing": false,
-                              "allow_symbol_change": true,
-                              "container_id": "tradingview_6446d"
-                            }
-                              );
-                                </script>
-                            </div>
                         <div className="AboutMission__image-container">
                             <img
                                 className="AboutMission__img"
