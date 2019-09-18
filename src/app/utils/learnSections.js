9ce7ca5b2fa368654bb96aa6899910fc0159a4c7
steemleo <52357015+steemleo@users.noreply.github.com>
2019-09-18 13:41:22 -0500

    Create learnSections.js

---
+++ src/app/utils/learnSections.js
@@ -0,0 +1,60 @@
+export const aboutGrow = `
+On Ulogs.org, it is **"true celebrity-hood for everyone, once and for all"**. We measure our success in terms of **'real human growth'**. We want to create **more value for 'humans'** in a world were cryptokitties, superman, millionaire-cats, human-clones etc is beginning to garner more value than 'humans'.
+&nbsp;
+&nbsp;
+Thus, we will incessantly seek innovative ways of inciting each ulogger to **'mine the human' into its awesomest version**. In the future, we will seek to celebrate a world filled with **'true celebrities'**, using the [TearDrops SMT](https://ulogs.org/teardrops/@surpassinggoogle/the-teardrops-smt-is-still-in-its-imaginary-state-but-it-is-time-to-start-gathering-read-this-and-consider-joining-our-telegram), 'a breakthrough token & emblem of human' that will reward 'proof of tears', for en-route attaining the awesomest version of human, there will be many tears, happy, sad or un-fell.
+&nbsp;
+&nbsp;
+We seek to remove 'all barriers to entry' from 'everything good', so that every(any)one can shine. The world needs more shine! @surpassinggoogle
+`;
+
+export const submitAVideo = `
+Across the ulogs.org ecosystem, we have 3 TV(s) i.e [UloggersTV](https://www.youtube.com/channel/UCzI3Rjamg7zSe_o0BwSeIQQ), [UntalentedTV](https://www.youtube.com/channel/UCq6ylwOaSG9VDYwZkMr3jMw) and [TeardropsTV](https://www.youtube.com/channel/UCnIJlzMZzfrcE0bXabLpXKw). We seek to use the media to impact the world and you can help matters. **We welcome your contributions**. We will feature your contribution on a corresponding TV. **In the future, your contributions may feature in our Uloggers Game App**.  
+&nbsp;
+&nbsp;
+While we only feature *'certified uloggers'* on our TV(s), if you are not yet a [*'certified ulogger'*](https://ulogs.org/discover) and you submit a video, **we will also look to feature your video** and once we do so, **you will become a 'certified ulogger'**.
+`;
+
+export const writeToUlogs = `
+The ulogs.org ecosystem also has an [un(dis)talented paradigm](https://ulogs.org/steemit/@surpassinggoogle/steemit-s-untalented-is-in-beta-participate-freely-because-every-participant-in-this-contest-will-win-something-no-losers). Thus, when you visit this space, **relegate reservations** and write to us freely. Whatever your dream is, we believe that in a **world of 'uloggers' (true celebrities) every noble dream is build-able**.  
+&nbsp;
+&nbsp;
+Tell us what you want and let's discuss. Don't undervalue the power of 'conversation', for at any moment, it can pop out something. Tell us of your forgotten dreams, let's resuscitate it. Do you need inspiration, assistance, mentoring, direction etc? Then, **write to us!** We are building an ecosystem of 'true celebrities', where everyone is your 'true fan'. At the very least, *'you are the celebrity; we are your fan'.*
+`;
+
+export const fifteenMinuteMentor = `
+On Ulogs.org, we will explore a **'variety of rewards'**. We are building our entire ecosystem around a ['grey-list of certified uloggers' (true celebrities)](https://ulogs.org/discover). This is an actual community of brothers, where each 'certified ulogger' is also a true fan. 
+&nbsp;
+&nbsp;
+Many times, we **rigamarole for years** seeking that *'ever-missing piece of the puzzle'*; sometimes a **simple conversation with 'a brother'** can turn *'years of rigmarole' into '15 mins of straight-to-the-point'*. Check out our **growing list of hand-picked mentors** under various niches and book a moment.
+`;
+
+export const thirtyMinuteSymposium = `
+**The ulogs.org ecosystem seeks to unite every steem community**, creating balance in the steem ecosystem. We are doing this using [ulog-subtags](https://ulogs.org/utopian-io/@surpassinggoogle/task-request-add-core-features-and-communities-to-ulogs-org-880-steem-bounty-and-more), which in turn grow into [ulog-communities](https://ulogs.org/utopian-io/@surpassinggoogle/task-request-add-core-features-and-communities-to-ulogs-org-880-steem-bounty-and-more) e.g [#ulog-helpie](https://ulogs.org/ulog-helpie), [#ulog-steemmonsters](https://ulogs.org/ulog-steemmonsters), [#ulog-actifit](https://ulogs.org/ulog-actifit) etc 
+&nbsp;
+&nbsp;
+We will try to help you get a **time allotment** on any of the popular steem community in our growing network, for you to talk about your projects in the midst of other steemians, during their discord talk shows. **Check out our available list and book a moment.**
+`;
+
+export const extraClout = `
+*You are a celebrity and we are your fan.* As we continue to **grow our network of 'certified uloggers'**, we may find some who have social influence whether on steem or outside steem. **Extra clout** goes a long way in creating awareness for your projects, dreams or you. **Clout** can take different forms too e.g a certified ulogger with some 1.7 million YouTube subscribers may be willing to give your product a shoutout on one of his viral videos etc. 
+&nbsp;
+&nbsp;
+Check out our list to **find out what our 'certified uloggers' have in stock for you**.
+`;
+
+export const extraConfidence = `
+Relegate reservations and reach out to us. We will seek ways to boost your confidence. [#ulog](https://ulogs.org/) represents a daily endeavor. On ulogs.org, each day carries its own freshness. **Thus, we are dynamically looking for ways to be your 'true fan'.**
+`;
+
+export const hardToExplainAilment = `
+It is worse kind of ailment, when it is a hard-to-explain ailment. **We understand.** We are growing a network of 'certified uloggers' who may understand. Reach out to us freely. **Please do!**
+`;
+
+export const sendUsSomething = `
+**You may want to send us something.** *We will give your our mailing address*. **We want to receive, for 'receiving is an art'.**
+`;
+
+export const inspireUs = `
+[#ulog](https://ulogs.org/) is a conscious effort to 'mine the human' into its awesomest version. This has to be a daily endeavor. You can inspire us. We need it too. Send us [ulogifs](https://ulogs.org/created/ulogifs), [ulog-memes](https://ulogs.org/created/ulog-memes), [ulog-quotes](https://ulogs.org/created/ulog-quotes), [ulography](https://ulogs.org/created/ulography) etc for the sole intention of inspiring us. **Courtesy of your own shine, we can find some shine too!**
+`;
