5842eafbc8c52b1fc0ea45d37534833eb52d95b1
Evan Chou <chou86.e@gmail.com>
2019-09-22 01:36:05 +0000

    revive zones

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -64,34 +64,34 @@ export const SUPPORT_EMAIL = 'support@' + APP_DOMAIN;
 // Revive Ads
 export const NO_ADS_STAKE_THRESHOLD = 2000;
 export const REVIVE_ADS = {
-    //header_banner: {
-    //    zoneId: '1699',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //sidebar_left: {
-    //    zoneId: '1767',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //sidebar_right: {
-    //    zoneId: '1761',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //post_footer_abovecomments: {
-    //    zoneId: '1768',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //post_footer_betweencomments: {
-    //    zoneId: '1769',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //feed: {
-    //    zoneId: '1777',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //feed_small: {
-    //    zoneId: '',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
+    header_banner: {
+        zoneId: '2036',
+        reviveId: '727bec5e09208690b050ccfc6a45d384',
+    },
+    sidebar_left: {
+        zoneId: '2037',
+        reviveId: '727bec5e09208690b050ccfc6a45d384',
+    },
+    sidebar_right: {
+        zoneId: '2038',
+        reviveId: '727bec5e09208690b050ccfc6a45d384',
+    },
+    post_footer_abovecomments: {
+        zoneId: '2039',
+        reviveId: '727bec5e09208690b050ccfc6a45d384',
+    },
+    post_footer_betweencomments: {
+        zoneId: '2040',
+        reviveId: '727bec5e09208690b050ccfc6a45d384',
+    },
+    feed: {
+        zoneId: '2041',
+        reviveId: '727bec5e09208690b050ccfc6a45d384',
+    },
+    feed_small: {
+        zoneId: '2042',
+        reviveId: '727bec5e09208690b050ccfc6a45d384',
+    },
 };
 
 // Other configurations
