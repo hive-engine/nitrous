cb4a62a1ae8bf5c5df5fc50a07266203c4e483e3
Evan Chou <chou86.e@gmail.com>
2019-09-24 04:34:27 +0000

    stake threshold 6000

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -67,7 +67,7 @@ export const SITE_DESCRIPTION =
 export const SUPPORT_EMAIL = 'support@' + APP_DOMAIN;
 
 // Revive Ads
-export const NO_ADS_STAKE_THRESHOLD = 2000;
+export const NO_ADS_STAKE_THRESHOLD = 6000;
 export const REVIVE_ADS = {
     header_banner: {
         zoneId: '2045',
