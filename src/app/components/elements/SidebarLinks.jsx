5fdc5ed489c2605c0432ddbb6f2ad65e3b1d21bd
donaldporter <53221067+donaldporter@users.noreply.github.com>
2019-09-25 15:46:31 -0400

    Removed A-Ads

--- src/app/components/elements/SidebarLinks.jsx
+++ src/app/components/elements/SidebarLinks.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import tt from 'counterpart';
 
-const adStyle = {
-    width: '200px',
-    height: '200px',
-    border: '0',
-    padding: '0', 
-    overflow:'hidden'
-};
-
 const SidebarLinks = ({ username }) => (
     <div className="c-sidebar__module">
         <div className="c-sidebar__header">
@@ -41,9 +33,6 @@ const SidebarLinks = ({ username }) => (
                     </a>
                 </li>
             </ul>
-        </div>
-        <div>
-            <iframe data-aa="1247599" src="//ad.a-ads.com/1247599?size=200x200" scrolling="no" style={adStyle} allowtransparency="true"></iframe>
         </div>
     </div>
 );
