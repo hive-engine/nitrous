0aeffdf76b4d90b1a7ca2fc21f8f69d4feacd6df
Evan Chou <chou86.e@gmail.com>
2019-09-26 11:49:16 +0000

    Revert "Merge pull request #159 from HappyBerrysBoy/scotty_steemcoinpan"
    This reverts commit a5c8e78387a1596ae324626c8743bb0f6eda0393, reversing
    changes made to 8e2b653186767be6bf02c994d23fbd85c1383390.

--- src/app/components/pages/Post.jsx
+++ src/app/components/pages/Post.jsx
@@ -185,12 +185,7 @@ class Post extends React.Component {
                         </div>
                     ) : null}
                     {this.props.reviveEnabled && showAd ? (
-                        <div className="Post_footer__ad show-for-mq-large">
-                            <ReviveAd adKey="post_footer_betweencomments" />
-                        </div>
-                    ) : null}
-                    {this.props.reviveEnabled && showAd ? (
-                        <div className="Post_footer__ad hide-for-mq-large">
+                        <div className="Post_footer__ad">
                             <ReviveAd adKey="feed_small" />
                         </div>
                     ) : null}
@@ -274,12 +269,7 @@ class Post extends React.Component {
                     </div>
                 ) : null}
                 {this.props.reviveEnabled ? (
-                    <div className="Post_footer__ad show-for-mq-large">
-                        <ReviveAd adKey="post_footer_abovecomments" />
-                    </div>
-                ) : null}
-                {this.props.reviveEnabled ? (
-                    <div className="Post_footer__ad hide-for-mq-large">
+                    <div className="Post_footer__ad">
                         <ReviveAd adKey="feed_small" />
                     </div>
                 ) : null}
