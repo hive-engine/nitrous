17e36a14e806168e3266ebf1ee2bbb1137629409
eonwarped <34953718+eonwarped@users.noreply.github.com>
2019-09-29 13:22:42 -0400

    Update Syndication.jsx

--- src/app/components/pages/Syndication.jsx
+++ src/app/components/pages/Syndication.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import HelpContent from 'app/components/elements/HelpContent';
 
-class Welcome extends React.Component {
+class Syndication extends React.Component {
     render() {
         return (
             <div className="row">
