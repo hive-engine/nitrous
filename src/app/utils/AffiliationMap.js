fca2aaca225b98450d643d991df69370b6090121
life3261 <55169907+life3261@users.noreply.github.com>
2019-10-01 09:51:20 -0500

    Update AffiliationMap.js

--- src/app/utils/AffiliationMap.js
+++ src/app/utils/AffiliationMap.js
@@ -1,15 +1,8 @@
 const map = {
-    //steemit
-    ned: 'Steemit',
-    justinw: 'Steemit',
-    elipowell: 'Steemit',
-    vandeberg: 'Steemit',
-    birdinc: 'Steemit',
-    gerbino: 'Steemit',
-    andrarchy: 'Steemit',
-    roadscape: 'Steemit',
-    steemitblog: 'Steemit',
-    steemitdev: 'Steemit',
+    //lifestyle team
+    'one.life': 'Official',
+    'nostyle4u': 'Moderator',
+    'khaleelkazi': 'Advisor',
 
     // Add Custom Badges. Use single quotes for the key if user has . or -,  e.g.
     // 'robot.pay' : 'Robot',
