82b1cf3605512731f7bda61328a95aee5fcb38cc
Evan <choue@google.com>
2019-12-14 23:15:45 +0000

    fix foodie tag

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -30,7 +30,7 @@ export const CONFIG_MAP = {
             sweets: [],
             vegetarian: [],
             vegan: [],
-            recipies: [],
+            recipes: [],
             community: [
                 'cleanplanet',
                 'innerblocks',
