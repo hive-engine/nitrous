22e0f768ac3cbd9c3b88ec8dc4bf5bd425471404
dporngithub <44737253+dporngithub@users.noreply.github.com>
2019-12-23 16:11:52 +0200

    Update TagFeedHeaderMap.js

--- src/app/utils/TagFeedHeaderMap.js
+++ src/app/utils/TagFeedHeaderMap.js
@@ -1,8 +1,7 @@
 const map = {
-    // default
-    'porn': 'This category covers all original porn on DPorn. Original amateur porn videos, porn pictures, porn gifs and porn stories. The hottest unique content for you.',
-    'boudoir': 'Welcome to **erotic boudoir gallery**! Boudoir - these are beautiful photos of gorgeous women in different atmospheric interiors. Each photo is a true work of art. Enjoy the photo **erotic boudoir photography**.',
+    // default	   
+    'porn': 'This category covers all original porn on DPorn. Original amateur porn videos, porn pictures, porn gifs and porn stories. The hottest unique content for you.',	    
     //'tag': "Tag description. Can use *Markdown*.",
-};
+};	
 
 export default map;
