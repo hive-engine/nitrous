ae70b1ce8f6a4c1429e5f5aa8676ab5c37b0b447
dporngithub <44737253+dporngithub@users.noreply.github.com>
2019-12-29 16:41:48 +0200

    Update TagFeedHeaderMap.js

--- src/app/utils/TagFeedHeaderMap.js
+++ src/app/utils/TagFeedHeaderMap.js
@@ -1,8 +1,21 @@
 const map = {
     // default	   
     'porn': 'This category covers all original porn on DPorn. Original amateur porn videos, porn pictures, porn gifs and porn stories. The hottest unique content for you.',
-    'boudoir': 'Welcome to **erotic boudoir gallery**! Boudoir - these are beautiful photos of gorgeous women in different atmospheric interiors. Each photo is a true work of art. Enjoy the photo **erotic boudoir photography**.',	    
+    'boudoir': 'Welcome to **erotic boudoir gallery**! Boudoir - these are beautiful photos of gorgeous women in different atmospheric interiors. Each photo is a true work of art. Enjoy the photo **erotic boudoir photography**.',
+    'erotica': 'Welcome to the erotic section! Here you will find useful articles, hot erotic stories, exciting erotic photos and erotic pictures from talented artists. Enjoy the beauty of the female body and be excited!',
+    'blowjob': 'There is nothing better than a good blowjob! In this category, photos and videos with a passionate and **hot blowjob**. These ladies know a lot about their business. Check out the posts and see for yourself. Awesome blowjob content will make you tremble with pleasure.',
+    'ass': 'Everyone loves good butt. This section of the ass for every taste: big, small, fit asses. What ass would you like to spank? Pretty ladies with nice asses really want someone to get hold of them.',
+    'pussy': 'A collection of most chic pussies of DPorn! Very impressive category. Hot pussy, shaved pussy, hairy pussy, cream pie. Porn with young girls with tight pussies and women with big experienced pussies. This is a real pleasure.',
+    'lesbian': 'Section with **hot lesbian girls**. They gently fuck each other. Lesbian porn is beautiful, enjoyable and exciting. These girls know how to lick a pussy and use sex toys. Enjoy lesbian porn and lesbian porn pics on DPorn.',
+    'ebony': 'Hot porn videos and photos with ebony girls. If you want something savory, this section is for you. **Ebony girls** with chocolate pussies and asses will surely satisfy your special needs.',
+    'creampie': 'Mmm. Sweet pussy - cream pie. Photos and porn from the cream pie category will excite you the most. There is nothing better than opening pussy like a rose and cum inside.',
+    'masturbation': 'This section is for **masturbation porn**. Its nice to watch how a cute girl appeals to herself or her friend or girlfriend. Stroking a pussy and bringing yourself to ecstasy is a pleasure, stroking pussy and massaging asshole is a double pleasure :)',
+    'fetish': 'This category covers everything related to the fetish. Bondage, BDSM, Sexual Dominated. It is pleasant, scary, anxious and exciting. Guys fuck their bitches or girls fuck guys and do whatever they want with their perverted mind with them. Fetish porn will make your evening unforgettable.',
+    'milf': '**Hot Milf Porn**. Here you will meet experienced milfs who know how to please their man. MILFs make an amazing blowjob and their pussies well take a big cock. Milfs have gorgeous asses and love being fucked from behind. Enjoy milf porn with DPorn.',
     //'tag': "Tag description. Can use *Markdown*.",
 };	
 
 export default map;
+
+
+
