6318c3dd5c9fe783e546ebbba1e7cedb588ce00e
eonwarped <34953718+eonwarped@users.noreply.github.com>
2020-01-17 19:12:10 +0800

    Update client_config.js

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -130,7 +130,7 @@ export const REVIVE_ADS = {
 export const ALLOW_MASTER_PW = false;
 // Footer to attach to posts. ${POST_URL} is a macro that can be used, will be expanded to the URL of the post.
 export const POST_FOOTER =
-    '<hr><center><sub>Posted via [Marlians.com](${POST_URL})</sub></center>';
+    "---\n<center><sub>Posted via [Marlians.com](${POST_URL})</sub></center>";
 export const SCOT_TAG_FIRST = false;
 export const SCOT_DEFAULT_BENEFICIARY_ACCOUNT = '';
 export const SCOT_DEFAULT_BENEFICIARY_PERCENT = 0; // between 0 amd 100
