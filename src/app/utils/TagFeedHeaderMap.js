25db1020c6e64f38635974cb9488c8334fb197e0
sportstalk <54014502+sportstalk@users.noreply.github.com>
2020-01-19 10:36:45 -0500

    Update TagFeedHeaderMap.js

--- src/app/utils/TagFeedHeaderMap.js
+++ src/app/utils/TagFeedHeaderMap.js
@@ -1,6 +1,6 @@
 const map = {
     // default
-    '': '',
+    'abuse': 'This tag is used by referees to track abuse of the SPORTS token.',
     //'tag': "Tag description. Can use *Markdown*.",
 };
 
