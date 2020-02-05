ade4aeb77f05e76b3c62c644dbddfb7a920d639a
swedishdragon76 <60705002+swedishdragon76@users.noreply.github.com>
2020-02-05 17:44:20 +0100

    Update AffiliationMap.js

--- src/app/utils/AffiliationMap.js
+++ src/app/utils/AffiliationMap.js
@@ -2,7 +2,7 @@ const map = {
     //steemit
     brumest: 'PhotoStreem Curator',
     swedishdragon: 'PhotoStreem Founder',
-    axeman: 'PhotoStreem Curator',
+    axeman: 
     cafeconleche: 'Photostreem Curator',
     nelyp: 'PhotoStreem Curator',
     derangedvisions: 'PhotoStreem Curator',
@@ -12,7 +12,7 @@ const map = {
     jznsamuel: 'PhotoStreem Curator',
     monochromes: 'PhotoStreem Supported Contest',
     portraitcontest: 'PhotoStreem Supported Contest',
-    roadstories: 'PhotoStreem Tribe Team',
+   
 
     // Add Custom Badges. Use single quotes for the key if user has . or -,  e.g.
     // 'robot.pay' : 'Robot',
