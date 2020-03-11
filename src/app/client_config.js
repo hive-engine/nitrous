fd385f7c745dbfc4e36a827d7a40454c2de501c8
eonwarped <34953718+eonwarped@users.noreply.github.com>
2020-03-11 11:42:51 -0400

    Update client_config.js

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -62,36 +62,24 @@ export const SITE_DESCRIPTION =
 export const SUPPORT_EMAIL = 'support@' + APP_DOMAIN;
 
 // Revive Ads
-export const NO_ADS_STAKE_THRESHOLD = 2000;
+export const NO_ADS_STAKE_THRESHOLD = 999999999999;
 export const REVIVE_ADS = {
-    //header_banner: {
-    //    zoneId: '1699',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //sidebar_left: {
-    //    zoneId: '1767',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //sidebar_right: {
-    //    zoneId: '1761',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //post_footer_abovecomments: {
-    //    zoneId: '1768',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //post_footer_betweencomments: {
-    //    zoneId: '1769',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //feed: {
-    //    zoneId: '1777',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
-    //feed_small: {
-    //    zoneId: '',
-    //    reviveId: '727bec5e09208690b050ccfc6a45d384',
-    //},
+    header_banner: {
+        zoneId: '4449',
+        reviveId: '727bec5e09208690b050ccfc6a45d384',
+    },
+    sidebar_left: {
+        zoneId: '4450',
+        reviveId: '727bec5e09208690b050ccfc6a45d384',
+    },
+    sidebar_right: {
+        zoneId: '4451',
+        reviveId: '727bec5e09208690b050ccfc6a45d384',
+    },
+    feed_small: {
+        zoneId: '4452',
+        reviveId: '727bec5e09208690b050ccfc6a45d384',
+    },
 };
 
 // Other configurations
