6736bd6e3aa8ecb2da14a8226bd4769bd69bc4bc
Evan Chou <chou86.e@gmail.com>
2020-03-13 22:33:41 +0000

    fix canonical linker test

--- src/app/utils/CanonicalLinker.test.js
+++ src/app/utils/CanonicalLinker.test.js
@@ -11,22 +11,22 @@ describe('makeCanonicalLink', () => {
         [
             'handles posts without app',
             { ...post_data, json_metadata: {} },
-            `www.appurl.com/testing/@test/test-post`,
+            `https://steemit.com/testing/@test/test-post`,
         ],
         [
             'handles empty strings as app',
             { ...post_data, json_metadata: { app: '' } },
-            `www.appurl.com/testing/@test/test-post`,
+            `https://steemit.com/testing/@test/test-post`,
         ],
         [
             "handles apps that don't exist",
             { ...post_data, json_metadata: { app: 'fakeapp/1.2.3' } },
-            `www.appurl.com/testing/@test/test-post`,
+            `https://steemit.com/testing/@test/test-post`,
         ],
         [
             "handles app that don't exist without version",
             { ...post_data, json_metadata: { app: 'fakeapp' } },
-            `www.appurl.com/testing/@test/test-post`,
+            `https://steemit.com/testing/@test/test-post`,
         ],
         [
             'handles apps that do exist',
@@ -44,27 +44,22 @@ describe('makeCanonicalLink', () => {
                 ...post_data,
                 json_metadata: { app: `appname/0.1` },
             },
-            `www.appurl.com/testing/@test/test-post`,
+            `https://steemit.com/testing/@test/test-post`,
         ],
         [
             'handles badly formatted app strings',
             { ...post_data, json_metadata: { app: 'fakeapp/0.0.1/a////' } },
-            `www.appurl.com/testing/@test/test-post`,
+            `https://steemit.com/testing/@test/test-post`,
         ],
         [
             'handles objects as apps',
             { ...post_data, json_metadata: { app: { this_is: 'an objct' } } },
-            `www.appurl.com/testing/@test/test-post`,
+            `https://steemit.com/testing/@test/test-post`,
         ],
     ];
     test_cases.forEach(v => {
         it(v[0], () => {
-            expect(
-                makeCanonicalLink(v[1], {
-                    APP_URL: 'www.appurl.com',
-                    APP_NAME: 'AppName',
-                })
-            ).toBe(v[2]);
+            expect(makeCanonicalLink(v[1])).toBe(v[2]);
         });
     });
 });
