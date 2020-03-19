1c019a6804ccb6f0267e96efe36397ac18738289
therealwolf42 <76-therealwolf@users.noreply.gitlab.syncad.com>
2020-03-19 11:15:40 +0100

    updated branding, adding links to sidebar, changed up a bit of css and updated a dependancy

--- src/app/components/elements/SidebarNewUsers.jsx
+++ src/app/components/elements/SidebarNewUsers.jsx
@@ -1,44 +1,81 @@
 import React from 'react';
 import tt from 'counterpart';
+import { connect } from 'react-redux';
 import { SIGNUP_URL } from 'shared/constants';
+import Icon from 'app/components/elements/Icon';
+import { Link } from 'react-router';
 
-const SidebarNewUsers = () => (
-    <div className="c-sidebar__module">
-        <div className="c-sidebar__header">
-            <h3 className="c-sidebar__h3">New to Hive?</h3>
-        </div>
-        <div className="c-sidebar__content">
-            <ul className="c-sidebar__list">
-                <li className="c-sidebar__list-item">
-                    <a className="c-sidebar__link" href="/welcome">
-                        Welcome Guide
+const SidebarNewUsers = ({ walletUrl }) => {
+    const makeLink = (i, ix, arr) => {
+        // A link is internal if it begins with a slash
+        const isExternal = !i.link.match(/^\//) || i.isExternal;
+        // const cn = ix === arr.length - 1 ? '--last' : 'c-sidebar__item';
+        if (isExternal) {
+            return (
+                <li key={ix} className={'c-sidebar__item'}>
+                    <a href={i.link} target="_blank" rel="noopener noreferrer">
+                        {i.label}&nbsp;<Icon name="extlink" />
                     </a>
                 </li>
-                {/*
-                <li className="c-sidebar__list-item">
-                    <a className="c-sidebar__link" href="/faq.html">
-                        FAQs
-                    </a>
+            );
+        }
+        if (i.onClick) {
+            return (
+                <li key={ix} className={cn}>
+                    <a onClick={i.onClick}>{i.label}</a>
                 </li>
-                <li className="c-sidebar__list-item">
-                    <a className="c-sidebar__link" href="/@steemitblog">
-                        {tt('g.read_offical_blog')}
-                    </a>
-                </li>
-                */}
-                <li className="c-sidebar__list-item">
-                    <a
-                        className="c-sidebar__link"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://hive.io/hive-whitepaper.pdf"
-                    >
-                        Whitepaper
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-);
+            );
+        }
+        return (
+            <li key={ix} className={cn}>
+                <Link to={i.link}>{i.label}</Link>
+            </li>
+        );
+    };
 
-export default SidebarNewUsers;
+    const links = [
+        {
+            label: tt('navigation.what_is_hive'),
+            link: 'https://hive.io',
+        },
+        {
+            label: tt('navigation.app_center'),
+            link: 'https://hivedapps.com/',
+        },
+        {
+            label: tt('navigation.blockexplorer'),
+            link: 'https://hiveblocks.com/',
+        },
+        /* {
+            label: tt('navigation.steem_engine'),
+            link: 'https://steemengine.com/',
+        },*/
+        {
+            label: tt('navigation.vote_for_witnesses'),
+            link: `${walletUrl}/~witnesses`,
+        },
+        {
+            label: tt('navigation.hive_proposals'),
+            link: `${walletUrl}/proposals`,
+        },
+    ];
+
+    return (
+        <div className="c-sidebar__module">
+            <div className="c-sidebar__header">
+                <h3 className="c-sidebar__h3">Explore Hive</h3>
+            </div>
+            <div className="c-sidebar__content">
+                <ul className="c-sidebar__list">{links.map(makeLink)}</ul>
+            </div>
+        </div>
+    );
+};
+
+export default connect((state, ownProps) => {
+    const walletUrl = state.app.get('walletUrl');
+    return {
+        walletUrl,
+        ...ownProps,
+    };
+})(SidebarNewUsers);
