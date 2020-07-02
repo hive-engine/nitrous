ed6f269bc7bd50c8b53f51e6b5defa1ed222fc00
eonwarped <34953718+eonwarped@users.noreply.github.com>
2020-07-02 12:42:50 -0400

    Update client_config.js

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -14,7 +14,18 @@ export const APP_DOMAIN = 'app.sandiegocoin.org';
 // max num of tags. if unset, default is 10. This is due to previous hardcoded number.
 export const APP_MAX_TAG = 10;
 export const SCOT_TAG = 'sandiego';
-export const TAG_LIST = fromJSOrdered(['sandiego']);
+export const TAG_LIST = fromJSOrdered([
+    'sandiego',
+    'houses',
+    'beach',
+    'tourism',
+    'government',
+    'frontyards',
+    'nature',
+    'military',
+    'business',
+    'marketplace', 
+]);
 export const LIQUID_TOKEN = 'Sand';
 // sometimes it's impossible to use html tags to style coin name, hence usage of _UPPERCASE modifier
 export const LIQUID_TOKEN_UPPERCASE = 'SAND';
