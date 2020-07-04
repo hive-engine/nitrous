9e823b51ee1cefa6972392b2d177d970bee2232d
sportstalk <54014502+sportstalk@users.noreply.github.com>
2020-07-04 13:11:36 -0400

    Change signup page
    Point signup page to signup.sportstalksocial.com

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -4,7 +4,7 @@ export const PARAM_VIEW_MODE = 'view_mode';
 export const VIEW_MODE_WHISTLE = 'whistle';
 export const WHISTLE_SIGNUP_COMPLETE = 'whistle_signup_complete';
 export const SIGNUP_URL = PREFER_HIVE
-    ? 'https://signup.hive.io'
+    ? 'http://signup.sportstalksocial.com'
     : 'https://signup.steemit.com';
 export const SUBMIT_FORM_ID = 'submitStory';
 export const PROMOTED_POST_PAD_SIZE = 5;
