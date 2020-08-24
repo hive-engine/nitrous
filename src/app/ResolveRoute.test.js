39e85618e7460264661054ba389497f5ed1fcb78
Evan Chou <chou86.e@gmail.com>
2020-08-24 21:03:41 +0000

    remove obsolete tests

--- src/app/ResolveRoute.test.js
+++ src/app/ResolveRoute.test.js
@@ -7,7 +7,7 @@ describe('routeRegex', () => {
             ['UserFeed', /^\/(@[\w\.\d-]+)\/feed\/?$/],
             [
                 'UserProfile',
-                /^\/(@[\w\.\d-]+)(?:\/(blog|posts|comments|replies|payout|feed|followed|followers|settings|notifications|communities|transfers))?\/?$/,
+                /^\/(@[\w\.\d-]+)(?:\/(blog|posts|comments|replies|payout|followed|followers|settings|notifications|communities|transfers))?\/?$/,
             ],
             [
                 'CategoryFilters',
