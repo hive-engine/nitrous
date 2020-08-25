27747f55a01dd0d5c468f4d32525a78797d4aed9
Evan Chou <chou86.e@gmail.com>
2020-08-25 15:39:34 +0000

    tag feed header map for sports talk social heading

--- src/app/utils/TagFeedHeaderMap.js
+++ src/app/utils/TagFeedHeaderMap.js
@@ -1,6 +1,7 @@
 const map = {
     // default
-    'abuse': 'This tag is used by referees to track abuse of the SPORTS token.',
+    '': '## Sports Talk Social: Latest Sports News and Discussions',
+    abuse: 'This tag is used by referees to track abuse of the SPORTS token.',
     //'tag': "Tag description. Can use *Markdown*.",
 };
 
