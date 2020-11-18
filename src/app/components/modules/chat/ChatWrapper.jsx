4c0d02ac412db88e392978b7e2219cde31074ad9
Evan Chou <chou86.e@gmail.com>
2020-11-18 14:42:42 +0000

    Add ability to select existing conversations + multiple rooms

--- src/app/components/modules/chat/ChatWrapper.jsx
+++ src/app/components/modules/chat/ChatWrapper.jsx
@@ -3,23 +3,36 @@ import { connect } from 'react-redux';
 import { ThemeProvider, FixedWrapper, darkTheme } from '@livechat/ui-kit';
 import MinimizedIcon from 'app/components/modules/chat/MinimizedIcon';
 import ChatMain from 'app/components/modules/chat/ChatMain';
-import { CHAT_CONVERSATION_ID } from 'app/client_config';
+import { CHAT_CONVERSATIONS } from 'app/client_config';
 
-class Chat extends React.Component {
+class ChatWrapper extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { conversation: null };
+    }
+
     render() {
         const { nightmodeEnabled } = this.props;
+        const { conversation } = this.state;
+        const setConversation = (conversation) => this.setState({ conversation });
         const theme = nightmodeEnabled ? darkTheme : { vars: { 'primary-color': 'grey' }};
-        if (!CHAT_CONVERSATION_ID || !this.props.currentUsername) {
+        theme.FixedWrapperMaximized = {
+            css: {
+                height: '600px',
+            },
+        };
+        if (!CHAT_CONVERSATIONS || !this.props.currentUsername) {
             return null;
         }
         return (
             <ThemeProvider theme={theme} >
                 <FixedWrapper.Root>
                     <FixedWrapper.Maximized>
-                        <ChatMain minimize={this.props.minimize} />
+                        <ChatMain conversation={conversation} setConversation={setConversation} />
                     </FixedWrapper.Maximized>
                     <FixedWrapper.Minimized>
-                        <MinimizedIcon maximize={this.props.maximize} />
+                        <MinimizedIcon />
                     </FixedWrapper.Minimized>
                 </FixedWrapper.Root>
             </ThemeProvider>
@@ -36,5 +49,5 @@ export default connect(
             nightmodeEnabled: state.app.getIn(['user_preferences', 'nightmode']),
         }
     },
-)(Chat);
+)(ChatWrapper);
 
