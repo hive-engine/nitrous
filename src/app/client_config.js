fee5ad00a686d203dcba56f356d52d9872816d21
Evan Chou <chou86.e@gmail.com>
2021-02-19 06:07:07 +0000

    gradnium config

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -797,10 +797,44 @@ const PIMP = {
     DISABLE_HIVE: true,
 };
 
+const GRADNIUM = {
+    APP_NAME: 'Gradnium',
+    APP_ICON: 'gradnium',
+    APP_ICON_WIDTH: '140px',
+    APP_ICON_HEIGHT: '40px',
+    APP_URL: 'https://www.gradnium.com',
+    APP_DOMAIN: 'www.gradnium.com',
+    LIQUID_TOKEN: 'Grad',
+    LIQUID_TOKEN_UPPERCASE: 'GRAD',
+    APP_MAX_TAG: 10,
+    SCOT_TAG: 'gradnium',
+    TAG_LIST: fromJSOrdered(['grad', 'gradnium']),
+    INTERLEAVE_PROMOTED: true,
+    PROMOTED_POST_ACCOUNT: 'null',
+    VESTING_TOKEN: 'GRAD POWER',
+    SITE_DESCRIPTION:
+        'Gradnium is a social media platform where everyone gets paid for ' +
+        'creating and curating content. It leverages a robust digital points system, called GRAD, that ' +
+        'supports real value for digital rewards through market price discovery and liquidity',
+    // Revive Ads
+    NO_ADS_STAKE_THRESHOLD: 9999999999,
+    REVIVE_ADS: {},
+    ALLOW_MASTER_PW: false,
+    // Footer to attach to posts. ${POST_URL} is a macro that can be used, will be expanded to the URL of the post.
+    POST_FOOTER: '',
+    COMMENT_FOOTER: '',
+    SCOT_TAG_FIRST: false,
+    SDC_GTAG_MEASUREMENT_ID: 'G-SH3WDSWXF8',
+    DISABLE_STEEM: true,
+    PREFER_HIVE: true,
+    HIVE_ENGINE: true,
+    CHAT_CONVERSATIONS: [],
+};
+
 export const CONFIG_MAP = {
     // testing heroku/local options
     'localhost:8080': HIVELIST,
-    'frozen-retreat-15997.herokuapp.com': PIMP,
+    'frozen-retreat-15997.herokuapp.com': GRADNIUM,
     'www.pimpstudio.cash': PIMP,
     'www.d-social.com': DSOCIAL,
     'blogtoken.io': BLOG,
