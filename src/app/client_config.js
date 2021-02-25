937be72dbeaad5b0adfe54fa0cc3c46a581a7ba5
Evan Chou <chou86.e@gmail.com>
2021-02-25 17:09:53 +0000

    reveleation icon updates, vibes assets and config

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -474,7 +474,7 @@ const HIVEHUSTLERS = {
 const REVELATION = {
     APP_NAME: 'ProjectRevelation',
     APP_ICON: 'projectrevelation',
-    APP_ICON_WIDTH: '40px',
+    APP_ICON_WIDTH: '166px',
     APP_ICON_HEIGHT: '40px',
     APP_URL: 'https://www.projectrevelation.io/',
     APP_DOMAIN: 'www.projectrevelation.io',
@@ -506,7 +506,46 @@ const REVELATION = {
     PREFER_HIVE: true,
     HIVE_ENGINE: true,
     CHAT_CONVERSATIONS: null,
-}
+    APPEND_TRENDING_TAGS_COUNT: 10,
+};
+
+const VIBES = {
+    APP_NAME: 'MusicForLife',
+    APP_ICON: 'musicforlife',
+    APP_ICON_WIDTH: '40px',
+    APP_ICON_HEIGHT: '40px',
+    APP_URL: 'https://www.musicforlife.io/',
+    APP_DOMAIN: 'www.musicforlife.io',
+    LIQUID_TOKEN: 'Vibes',
+    LIQUID_TOKEN_UPPERCASE: 'VIBES',
+    APP_MAX_TAG: 10,
+    SCOT_TAG: 'musicforlife',
+	TAG_LIST: fromJSOrdered([
+		'musicforlife',
+    ]),
+    INTERLEAVE_PROMOTED: true,
+    PROMOTED_POST_ACCOUNT: 'null',
+    VESTING_TOKEN: 'VIBES POWER',
+    SITE_DESCRIPTION:
+        'MusicForLife is a social media platform where everyone gets paid for ' +
+        'creating and curating content. It leverages a robust digital points system, called VIBES, that ' +
+        'supports real value for digital rewards through market price discovery and liquidity',
+    // Revive Ads
+    NO_ADS_STAKE_THRESHOLD: 9999999999,
+    REVIVE_ADS: {},
+    ALLOW_MASTER_PW: false,
+    // Footer to attach to posts. ${POST_URL} is a macro that can be used, will be expanded to the URL of the post.
+    POST_FOOTER: '',
+    COMMENT_FOOTER: '',
+    SCOT_TAG_FIRST: false,
+    PINNED_POSTS_URL: '',
+    SDC_GTAG_MEASUREMENT_ID: 'G-8SB0V2MDLS',
+    DISABLE_STEEM: true,
+    PREFER_HIVE: true,
+    HIVE_ENGINE: true,
+    CHAT_CONVERSATIONS: null,
+    APPEND_TRENDING_TAGS_COUNT: 10,
+};
 
 const ARCHON = {
     APP_NAME: 'ArchonApp',
@@ -925,7 +964,8 @@ const GRADNIUM = {
 export const CONFIG_MAP = {
     // testing heroku/local options
     'localhost:8080': HIVEHUSTLERS,
-    'frozen-retreat-15997.herokuapp.com': REVELATION,
+    'frozen-retreat-15997.herokuapp.com': VIBES,
+    'www.musicforlife.io': VIBES,
     'www.projectrevelation.io': REVELATION,
     'www.hivehustlers.io': HIVEHUSTLERS,
     'www.gradnium.com': GRADNIUM,
