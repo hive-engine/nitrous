93198c92e5e55795a118a708959e31bfa56fe3bb
Evan Chou <chou86.e@gmail.com>
2021-04-12 17:53:03 +0000

    lern assets and config

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -240,6 +240,46 @@ const REVELATION = {
     COMMUNITY_CATEGORY: 'hive-198141',
 };
 
+const LERN = {
+    APP_NAME: 'LernHerstory',
+    APP_ICON: 'lern',
+    APP_ICON_WIDTH: '70px',
+    APP_ICON_HEIGHT: '40px',
+    APP_URL: 'https://www.lernblogs.com/',
+    APP_DOMAIN: 'www.lernblogs.com',
+    LIQUID_TOKEN: 'Lern',
+    LIQUID_TOKEN_UPPERCASE: 'LERN',
+    APP_MAX_TAG: 10,
+    SCOT_TAG: 'lern',
+	TAG_LIST: fromJSOrdered([
+		'lern',
+        'herstory',
+    ]),
+    INTERLEAVE_PROMOTED: true,
+    PROMOTED_POST_ACCOUNT: 'null',
+    VESTING_TOKEN: 'LERN POWER',
+    SITE_DESCRIPTION:
+        'LernHerstory is a social media platform where everyone gets paid for ' +
+        'creating and curating content. It leverages a robust digital points system, called LERN, that ' +
+        'supports real value for digital rewards through market price discovery and liquidity',
+    // Revive Ads
+    NO_ADS_STAKE_THRESHOLD: 9999999999,
+    REVIVE_ADS: {},
+    ALLOW_MASTER_PW: false,
+    // Footer to attach to posts. ${POST_URL} is a macro that can be used, will be expanded to the URL of the post.
+    POST_FOOTER: '',
+    COMMENT_FOOTER: '',
+    SCOT_TAG_FIRST: false,
+    PINNED_POSTS_URL: '',
+    SDC_GTAG_MEASUREMENT_ID: 'G-TX4HJ2V8VM',
+    DISABLE_STEEM: true,
+    PREFER_HIVE: true,
+    HIVE_ENGINE: true,
+    CHAT_CONVERSATIONS: null,
+    APPEND_TRENDING_TAGS_COUNT: 10,
+    COMMUNITY_CATEGORY: 'hive-186927',
+};
+
 const VIBES = {
     APP_NAME: 'MusicForLife',
     APP_ICON: 'musicforlife',
@@ -790,12 +830,13 @@ const BROADHIVE = {
 
 export const CONFIG_MAP = {
     // testing heroku/local options
-    'localhost:8080': PHOTO,
-    'frozen-retreat-15997.herokuapp.com': PHOTO,
+    'localhost:8080': LERN,
+    'frozen-retreat-15997.herokuapp.com': LERN,
     'www.photostreem.com': PHOTO,
     'www.broadhive.org': BROADHIVE,
     'www.cinetv.io': CINETV,
     'www.proofofbrain.io': POB,
+    'www.lernblogs.com': LERN,
     'www.musicforlife.io': VIBES,
     'www.projectrevelation.io': REVELATION,
     'www.hivehustlers.io': HIVEHUSTLERS,
