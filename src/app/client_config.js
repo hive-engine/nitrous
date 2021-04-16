7ec1df9ac7780b13cb56ab72dffd3f5d29b7fa2b
Evan Chou <chou86.e@gmail.com>
2021-04-16 13:10:48 +0000

    alive config and assets

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -280,6 +280,45 @@ const LERN = {
     COMMUNITY_CATEGORY: 'hive-186927',
 };
 
+const ALIVE = {
+    APP_NAME: 'WeAreAliveAndSocial',
+    APP_ICON: 'alive',
+    APP_ICON_WIDTH: '180px',
+    APP_ICON_HEIGHT: '40px',
+    APP_URL: 'https://www.wearealiveand.social/',
+    APP_DOMAIN: 'www.wearealiveand.social',
+    LIQUID_TOKEN: 'Alive',
+    LIQUID_TOKEN_UPPERCASE: 'ALIVE',
+    APP_MAX_TAG: 10,
+    SCOT_TAG: 'alive',
+	TAG_LIST: fromJSOrdered([
+		'alive',
+    ]),
+    INTERLEAVE_PROMOTED: true,
+    PROMOTED_POST_ACCOUNT: 'null',
+    VESTING_TOKEN: 'ALIVE POWER',
+    SITE_DESCRIPTION:
+        'WeAreAliveAndSocial is a social media platform where everyone gets paid for ' +
+        'creating and curating content. It leverages a robust digital points system, called ALIVE, that ' +
+        'supports real value for digital rewards through market price discovery and liquidity',
+    // Revive Ads
+    NO_ADS_STAKE_THRESHOLD: 9999999999,
+    REVIVE_ADS: {},
+    ALLOW_MASTER_PW: false,
+    // Footer to attach to posts. ${POST_URL} is a macro that can be used, will be expanded to the URL of the post.
+    POST_FOOTER: '',
+    COMMENT_FOOTER: '',
+    SCOT_TAG_FIRST: false,
+    PINNED_POSTS_URL: '',
+    SDC_GTAG_MEASUREMENT_ID: 'G-2YXSKZNZFS',
+    DISABLE_STEEM: true,
+    PREFER_HIVE: true,
+    HIVE_ENGINE: true,
+    CHAT_CONVERSATIONS: null,
+    APPEND_TRENDING_TAGS_COUNT: 10,
+    COMMUNITY_CATEGORY: 'hive-155221',
+};
+
 const VIBES = {
     APP_NAME: 'MusicForLife',
     APP_ICON: 'musicforlife',
@@ -830,8 +869,9 @@ const BROADHIVE = {
 
 export const CONFIG_MAP = {
     // testing heroku/local options
-    'localhost:8080': LERN,
-    'frozen-retreat-15997.herokuapp.com': LERN,
+    'localhost:8080': ALIVE,
+    'frozen-retreat-15997.herokuapp.com': ALIVE,
+    'www.wearealiveand.social': ALIVE,
     'www.photostreem.com': PHOTO,
     'www.broadhive.org': BROADHIVE,
     'www.cinetv.io': CINETV,
