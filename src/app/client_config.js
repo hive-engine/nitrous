77471a8d8f4fbbe0c3e711202313b1bbc2739202
Evan Chou <chou86.e@gmail.com>
2021-08-06 16:36:48 +0000

    hesmt config for BUIDL

--- src/app/client_config.js
+++ src/app/client_config.js
@@ -1,23 +1,23 @@
 import { fromJSOrdered } from './utils/immutable';
 
-const DUNK = {
-    APP_NAME: 'DunkSocial',
-    APP_ICON: 'dunksocial',
-    APP_ICON_WIDTH: '50px',
+const BUIDL = {
+    APP_NAME: 'BuildIt',
+    APP_ICON: 'build-it',
+    APP_ICON_WIDTH: '150px',
     APP_ICON_HEIGHT: '40px',
-    APP_URL: 'https://www.dunksocial.io',
-    APP_DOMAIN: 'www.dunksocial.io',
-    LIQUID_TOKEN: 'DUNK',
-    LIQUID_TOKEN_UPPERCASE: 'DUNK',
+    APP_URL: 'https://www.build-it.io',
+    APP_DOMAIN: 'www.build-it.io',
+    LIQUID_TOKEN: 'BUIDL',
+    LIQUID_TOKEN_UPPERCASE: 'BUIDL',
     APP_MAX_TAG: 10,
-    SCOT_TAG: 'dunk',
-    TAG_LIST: fromJSOrdered(['dunk', 'dunksocial']),
+    SCOT_TAG: 'build-it',
+    TAG_LIST: fromJSOrdered(['build-it', 'buidl', 'diy']),
     INTERLEAVE_PROMOTED: true,
     PROMOTED_POST_ACCOUNT: 'null',
-    VESTING_TOKEN: 'DUNK POWER',
+    VESTING_TOKEN: 'BUIDL POWER',
     SITE_DESCRIPTION:
-        'DunkSocial is a social media platform where everyone gets paid for ' +
-        'creating and curating content. It leverages a robust digital points system, called DUNK, that ' +
+        'BuildIt is a social media platform where everyone gets paid for ' +
+        'creating and curating content. It leverages a robust digital points system, called BUIDL, that ' +
         'supports real value for digital rewards through market price discovery and liquidity',
     // Revive Ads
     NO_ADS_STAKE_THRESHOLD: 9999999999,
@@ -27,100 +27,25 @@ const DUNK = {
     POST_FOOTER: '',
     COMMENT_FOOTER: '',
     SCOT_TAG_FIRST: false,
-    SDC_GTAG_MEASUREMENT_ID: 'G-LHBWFW0JD5',
+    SDC_GTAG_MEASUREMENT_ID: 'UA-145448693-22',
     DISABLE_STEEM: true,
     PREFER_HIVE: true,
     HIVE_ENGINE: true,
-    HIVE_ENGINE_SMT: 1,
+    HIVE_ENGINE_SMT: 5,
     APPEND_TRENDING_TAGS_COUNT: 10,
-    COMMUNITY_CATEGORY: 'hive-195254',
-    CHAT_CONVERSATIONS: null,
-    PINNED_POSTS_URL: 'https://robminnick23.github.io/',
-};
-
-const CENT = {
-    APP_NAME: 'CentSocial',
-    APP_ICON: 'cent',
-    APP_ICON_WIDTH: '40px',
-    APP_ICON_HEIGHT: '40px',
-    APP_URL: 'https://www.centblog.org',
-    APP_DOMAIN: 'www.centblog.io',
-    LIQUID_TOKEN: 'CENT',
-    LIQUID_TOKEN_UPPERCASE: 'CENT',
-    APP_MAX_TAG: 10,
-    SCOT_TAG: 'cent',
-    TAG_LIST: fromJSOrdered(['cent', 'economy']),
-    INTERLEAVE_PROMOTED: true,
-    PROMOTED_POST_ACCOUNT: 'null',
-    VESTING_TOKEN: 'CENT POWER',
-    SITE_DESCRIPTION:
-        'CentSocial is a social media platform where everyone gets paid for ' +
-        'creating and curating content. It leverages a robust digital points system, called CENT, that ' +
-        'supports real value for digital rewards through market price discovery and liquidity',
-    // Revive Ads
-    NO_ADS_STAKE_THRESHOLD: 9999999999,
-    REVIVE_ADS: {},
-    ALLOW_MASTER_PW: false,
-    // Footer to attach to posts. ${POST_URL} is a macro that can be used, will be expanded to the URL of the post.
-    POST_FOOTER: '',
-    COMMENT_FOOTER: '',
-    SCOT_TAG_FIRST: false,
-    SDC_GTAG_MEASUREMENT_ID: 'G-HSQW2JJ769',
-    DISABLE_STEEM: true,
-    PREFER_HIVE: true,
-    HIVE_ENGINE: true,
-    HIVE_ENGINE_SMT: 2,
-    APPEND_TRENDING_TAGS_COUNT: 10,
-    COMMUNITY_CATEGORY: 'hive-173575',
-    CHAT_CONVERSATIONS: null,
-};
-
-const BEATCZ = {
-    APP_NAME: 'Beatzchain',
-    APP_ICON: 'beatcz',
-    APP_ICON_WIDTH: '40px',
-    APP_ICON_HEIGHT: '40px',
-    APP_URL: 'https://www.beatzchain.com',
-    APP_DOMAIN: 'www.beatzchain.com',
-    LIQUID_TOKEN: 'BEATCZ',
-    LIQUID_TOKEN_UPPERCASE: 'BEATCZ',
-    APP_MAX_TAG: 10,
-    SCOT_TAG: 'beatcz',
-    TAG_LIST: fromJSOrdered(['beatzchain', 'beatcz' ]),
-    INTERLEAVE_PROMOTED: true,
-    PROMOTED_POST_ACCOUNT: 'null',
-    VESTING_TOKEN: 'BEATCZ POWER',
-    SITE_DESCRIPTION:
-        'Beatzchain is a social media platform where everyone gets paid for ' +
-        'creating and curating content. It leverages a robust digital points system, called BEATCZ, that ' +
-        'supports real value for digital rewards through market price discovery and liquidity',
-    // Revive Ads
-    NO_ADS_STAKE_THRESHOLD: 9999999999,
-    REVIVE_ADS: {},
-    ALLOW_MASTER_PW: false,
-    // Footer to attach to posts. ${POST_URL} is a macro that can be used, will be expanded to the URL of the post.
-    POST_FOOTER: '',
-    COMMENT_FOOTER: '',
-    SCOT_TAG_FIRST: false,
-    SDC_GTAG_MEASUREMENT_ID: 'G-JEF024D6TZ',
-    DISABLE_STEEM: true,
-    PREFER_HIVE: true,
-    HIVE_ENGINE: true,
-    HIVE_ENGINE_SMT: 3,
-    APPEND_TRENDING_TAGS_COUNT: 10,
-    COMMUNITY_CATEGORY: 'hive-123620',
-    CHAT_CONVERSATIONS: null,
-    DEFAULT_URL: '/created',
+    COMMUNITY_CATEGORY: 'hive-129017',
+    CHAT_CONVERSATIONS: [
+        { id: '01EPB6A2PPSW0BQVJ7WDDP568C', name: 'BeeChat Trollbox' },
+    ],
+    POSTED_VIA_NITROUS_ICON: 'buildcaravan',
+    SHOW_AUTHOR_RECENT_POSTS: true,
+    SHOW_TOKEN_STATS: true,
 };
 
 export const CONFIG_MAP = {
     // testing heroku/local options
-    'localhost:8080': CENT,
-    'eonhetest.herokuapp.com': BEATCZ,
-    'www.dunksocial.io': DUNK,
-    'dunksocial.io': DUNK,
-    'www.centblog.org': CENT,
-    'www.beatzchain.com': BEATCZ,
+    'localhost:8080': BUIDL,
+    'www.build-it.io': BUIDL,
 };
 
 export const HIVE_SIGNER_APP = 'ewd';
