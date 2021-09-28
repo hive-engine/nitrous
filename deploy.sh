01ebde042088bb4da5b22949247857849d703ed1
Evan Chou <chou86.e@gmail.com>
2021-09-28 18:29:13 +0000

    remove deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-sudo docker build -t registry.heroku.com/frozen-retreat-15997/web .
-
-sudo docker push registry.heroku.com/frozen-retreat-15997/web:latest
-
-heroku container:release web --app frozen-retreat-15997
