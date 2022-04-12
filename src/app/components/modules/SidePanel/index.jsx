85cb43f5c1acbf73a0be8679c625dfc1ea1ecdeb
Abod Michael <abodmicheal@gmail.com>
2022-04-12 20:32:10 +0100

    LitePaper Update

--- src/app/components/modules/SidePanel/index.jsx
+++ src/app/components/modules/SidePanel/index.jsx
@@ -64,7 +64,7 @@ const SidePanel = ({
             {
                 value: 'engine',
                 label: 'LitePaper',
-                link: ' https://build-it.blog/builditassets/buildit-litepaper.pdf',
+                link: 'https://eu.docworkspace.com/d/sIHWUtOWLAYXB1pIG',
             },
             {
                 value: 'engine',
