7fb3a11b3338db77643c6b229dcd9243d98dc8d4
Abod Michael <abodmicheal@gmail.com>
2022-04-18 09:53:47 +0100

    new patch

--- src/app/components/modules/SidePanel/index.jsx
+++ src/app/components/modules/SidePanel/index.jsx
@@ -66,8 +66,7 @@ const SidePanel = ({
             {
                 value: 'engine',
                 label: 'LitePaper',
-                link:
-                    ' https://build-it.blog/builditassets/buildit-litepaper.pdf',
+                link: 'https://build-it.blog/builditassets/buildit-litepaper.pdf',
             },
             {
                 value: 'engine',
