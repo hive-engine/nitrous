a2ad27e615ac5afd6fdab1821ad6d9bf4b8f0794
Abod Michael <abodmicheal@gmail.com>
2022-05-05 16:58:14 +0100

    new test

--- src/app/components/DarkMode/DarkModeBtn.jsx
+++ src/app/components/DarkMode/DarkModeBtn.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import moondark from './moon.png';
-import moonlight from './moon_light.png';
 const DarkModeBtn = ({ toggleBody }) => {
-    return (
+    return ( 
         <div style={{ marginLeft: '20px' }}>
             <button
                 id="darkMode"
                 style={{ cursor: 'pointer' }}
                 onClick={e => toggleBody(e)}
             >
-                {' '}
                 <div id="moon">
                     <img
                         src={moondark}
-                        alt="moonlight"
+                        alt="moon"
                         width="40px"
                         style={{ marginLeft: '10px' }}
                     />
